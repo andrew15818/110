c43995396aa6f1ab82f0b77f9aac7b687a8e0a2e
Andres Ponce <andreseeponce@gmail.com>
2021-12-31 16:50:18 +0000

    Init

--- fall/data_mining/homework/hw3/algos/pagerank.py
+++ fall/data_mining/homework/hw3/algos/pagerank.py
@@ -1,3 +1,13 @@
+import numpy as np
+from graph import Graph
+
 class PageRank:
-    def __init__(self):
-        pass
+    def __init__(self, damping):
+        self.damping = damping
+
+    def run(self, graph:Graph):
+        N = len(graph.vertices)
+        # Use the children of each vertex for adj. matrix
+        adj = graph.adjacency_matrix()
+        # TODO: Get the pagerank value for each child
+        
