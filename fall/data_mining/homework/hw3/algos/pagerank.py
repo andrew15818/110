f87aeeae5b314dd63f3ac68496539d0b21973392
Andres Ponce <andreseeponce@gmail.com>
2022-01-04 13:32:07 +0000

    calculate transition matrix

--- fall/data_mining/homework/hw3/algos/pagerank.py
+++ fall/data_mining/homework/hw3/algos/pagerank.py
@@ -5,9 +5,23 @@ class PageRank:
     def __init__(self, damping):
         self.damping = damping
 
-    def run(self, graph:Graph):
+    # TODO: Get rid of the iterations and 
+    # figure out when algo converges
+    def run(self, graph:Graph, iterations:int):
         N = len(graph.vertices)
         # Use the children of each vertex for adj. matrix
         adj = graph.adjacency_matrix()
-        # TODO: Get the pagerank value for each child
+
+        print(np.linalg.norm(adj,1 ))
+        # Get transition matrix
+        M = []
+        for i, row in enumerate(adj):
+            count = (row==1).sum()
+            M.append(row/count if count > 0 else row)
+        
+        for i in range(iterations):
+            pass
+
+
+
         
