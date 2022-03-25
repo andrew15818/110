cd984a59a12bc6507290f80dd10eb83a1b303b89
Andres Ponce <andreseeponce@gmail.com>
2022-03-25 13:00:35 +0800

    function skeletons

--- spring/optimization/homework/hw2/main.py
+++ spring/optimization/homework/hw2/main.py
@@ -1,7 +1,14 @@
+import math
 import matplotlib.pyplot as plt
+import  missingno
+
+def f1() -> float:
+    return (x ** 3) * math.exp(-(x ** 2))
+def p1(start, end):
+    pass
 
 def main():
-    pass
+    p1(-2, 2)
 
 if __name__== 'main':
     main()
