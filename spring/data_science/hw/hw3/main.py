84671ce499a38a033066a1690bd0e4016f9705e5
Andres Ponce <andreseeponce@gmail.com>
2022-03-28 12:51:48 +0000

    Remove values from array

--- spring/data_science/hw/hw3/main.py
+++ spring/data_science/hw/hw3/main.py
@@ -1,13 +1,12 @@
 import os
 import glob
 import argparse
+import math
+import random
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-# TODO: Load the csvs
-# Normalize the features
-# Remove the percentage from the features
 parser = argparse.ArgumentParser(
             usage='%(prog) [OPTIONS]',
             description='Practice regression with missing values.'
@@ -41,7 +40,8 @@ def get_rows(filename:str, sep=None):
 # Open file, return full dataset
 # Sep is because some files use tabs, others spaces
 def get_dataset() -> np.array:
-    tabs = ['power', 'energy']
+    tabs = ['power', 'energy'] # These files use tabs to separate values
+    # All datasets are in their own dir
     for filename in glob.iglob(f'{args.datadir}/*.txt'):
         sep = None
         for tabfile in tabs:
@@ -50,14 +50,29 @@ def get_dataset() -> np.array:
         rows = get_rows(filename, sep=sep)
         yield rows 
 
+def remove_feature_values(features:np.array, ratio=0.3):
+    hideNum = int(features.size * ratio)
+    print(f'Removing {features.size}*{ratio} = {hideNum}')
+    for i in range(hideNum):
+        hrow = random.randint(0, features.shape[0]) - 1
+        hcol = random.randint(0, features.shape[1]) - 1
+        features[hrow][hcol] = math.nan
+        print(features[hrow])
+    
+    
+
+
 def main():
-    missing_ratios = [0.1, 0.3, 0.5, 0.7]
+    missingRatios = [0.1, 0.3, 0.5, 0.7]
     for dataset in get_dataset():
-        print(dataset.shape)
-        #features = dataset[:,:-1]
-        #labels = dataset[:,-1]
-        #print(features.shape, labels.shape)
-        pass
+
+        copy = dataset.copy()
+        # Only remove from the features
+        features, labels  = copy[:,:-1], copy[:,-1]
+
+        remove_feature_values(features)
+        print(features)
+        break
 
 if __name__=='__main__':
     main()
