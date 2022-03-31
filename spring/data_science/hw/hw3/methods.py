b7accf090ab9783fc0a130eef2988faec6e65c68
Andres Ponce <andreseeponce@gmail.com>
2022-03-31 17:49:28 +0800

    non-ML imputers

--- spring/data_science/hw/hw3/methods.py
+++ spring/data_science/hw/hw3/methods.py
@@ -1,9 +1,20 @@
 import numpy as np
 import sklearn.impute as impute
+# Need this since IterativeImputer is still experimental
+from sklearn.experimental import enable_iterative_imputer
 
 def mean_imp(features):
     imputer = impute.SimpleImputer(missing_values=np.nan,
                                     strategy='mean')
+    return imputer.fit_transform(features)
+
+def knn(features, n=5):
+    imputer = impute.KNNImputer(missing_values=np.nan, 
+                                    n_neighbors=n)
+    return imputer.fit_transform(features)
+
+def mice(features):
+    imputer = impute.IterativeImputer(missing_values=np.nan)
     return imputer.fit_transform(features)
 
 
