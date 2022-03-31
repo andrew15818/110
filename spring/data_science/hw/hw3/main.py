e5b2e06d7aa0c27252fc608546282f70e4c8cdf6
Andres Ponce <andreseeponce@gmail.com>
2022-03-31 15:00:10 +0800

    plot missing values, rough structure for main

--- spring/data_science/hw/hw3/main.py
+++ spring/data_science/hw/hw3/main.py
@@ -3,12 +3,16 @@ import glob
 import argparse
 import math
 import random
+import missingno as msno
 import numpy as np
 import pandas as pd
-import methods
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
+import methods
+TRIAL_NO = 5 # Number of runs per missing ratio
 
 parser = argparse.ArgumentParser(
             usage='%(prog) [OPTIONS]',
@@ -40,6 +44,10 @@ def get_rows(filename:str, sep=None):
             rows.append(line)
         return np.array(rows, dtype=np.float64)
 
+
+def mae(preds:np.array, labels:np.array):
+    return abs(preds - labels) / preds.shape[0]
+
 # Open file, return full dataset
 # Some files are separated by spaces, others w/ tabs
 def get_dataset() -> np.array:
@@ -51,7 +59,8 @@ def get_dataset() -> np.array:
             sep = '\t' if tabfile in filename else sep
         print(filename)
         rows = get_rows(filename, sep=sep)
-        yield rows 
+        yield rows, filename
+
 
 def remove_feature_values(features:np.array, ratio=0.3):
     hideNum = int(features.size * ratio)
@@ -69,22 +78,48 @@ def preprocess(features, labels):
     scaler.fit_transform(x_train, y_train) 
     return x_train, x_test, y_train, y_test
 
+
+def plot_missing_values(features:np.array):
+    msno.matrix(pd.DataFrame(features))
+    plt.show()
+
+
+def plot_error(errors, ratios=[0.1, 0.3, 0.5, 0.7]):
+    pass
+
 def main():
     missingRatios = [0.1, 0.3, 0.5, 0.7]
-    for dataset in get_dataset():
+    total_errors = {}
+    for dataset, filename in get_dataset():
+        for ratio in missingRatios:
+            ratio_error = 0
+            for i in range(5):
 
-        copy = dataset.copy()
-        # Only remove from the features
-        features, labels  = copy[:,:-1], copy[:,-1]
+                copy = dataset.copy()
+                # Only remove from the features
+                features, labels  = copy[:,:-1], copy[:,-1]
 
-        remove_feature_values(features)
-        x_train, x_test, y_train, y_test = preprocess(features, labels)
-        imputed = methods.mean_imp(x_train)
+                remove_feature_values(features)
+                x_train, x_test, y_train, y_test = preprocess(features, labels)
+                imputed_train = methods.mean_imp(x_train)
+                imputed_test = methods.mean_imp(x_test) 
 
-        # Train a linear regression model
-        model = LinearRegression()
-        model.fit(imputed, y_train)
+                # Train a linear regression model
+                model = LinearRegression()
+                model.fit(imputed_train, y_train)
+
+                # Get our predictions with imputed data
+                preds = model.predict(imputed_test)
+                error = mae(preds, y_test)
+                print(f'Test {i+1}: Missing data: {ratio}: \
+                    error: {error.mean()}')
+
+                ratio_error += error.mean()
+
+        total_errors[filename] = ratio_error / TRIAL_NO
+
         break # debug
+
 
 if __name__=='__main__':
     main()
