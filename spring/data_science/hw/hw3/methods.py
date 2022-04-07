999ab49c5b426c0a36948e74c12392e06c164ef0
Andres Ponce <andreseeponce@gmail.com>
2022-04-07 14:12:05 +0000

    add std

--- spring/data_science/hw/hw3/methods.py
+++ spring/data_science/hw/hw3/methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import sklearn.impute as impute
 from GAIN.gain import gain as gain_imp
 #from GRAPE.models.gnn_model import get_gnn
@@ -20,6 +21,22 @@ def mice(features):
     imputer = impute.IterativeImputer(missing_values=np.nan)
     return imputer.fit_transform(features)
 
+# Sample random numbers within standard deviation
+def std(features):
+    phi = np.std(features, axis=1)
+    imputer = impute.SimpleImputer(missing_values=np.nan, 
+                                    fill_value=phi[0]*np.random.sample()
+                                    )
+    arr = imputer.fit_transform(features[:,0].reshape(-1,1))
+    for idx in range(1, features.shape[1]):
+        col = features[:,idx]
+        imputer = impute.SimpleImputer(missing_values=np.nan, 
+                                        fill_value=(col.mean() + phi[idx]*np.random.sample()))
+        imputed = imputer.fit_transform(features[:,idx].reshape(-1,1))
+        arr = np.concatenate((arr, imputed),  axis=1)
+    return arr 
+        
+
 # I think we first do the missing indicator, then impute the values?
 def missing_indicator(features, method='mean'):
     imputer = impute.MissingIndicator(missing_values=np.nan)
@@ -31,17 +48,18 @@ def missing_indicator(features, method='mean'):
         imputed = knn(features)
     else:
         imputed = mice(features)
-
     # Add the missing indicators as extra columns
-    return np.concatenate((imputed, mask), axis=1)
 
-def gain(features):
-    # Settings taken from GAIN/main_letter_spam.py
-    # TODO: Find a way to not just use defaults?
-    parameters = {'batch_size': 128,
-            'hint_rate': 0.9,
+    out = np.concatenate((imputed, mask), axis=1)
+    return out
+def gain(features, missingRatio=0.1):
+    # Using batch_size=128 gave me dimension error on one dataset?
+    # This batch_size works
+    parameters = {'batch_size': 93,
+            # missing data = 1 - hint_rate
+            'hint_rate': 1-missingRatio,
             'alpha': 100,
-            'iterations': 10000
+            'iterations': 1
             }
     return gain_imp(features, parameters)
 
@@ -55,8 +73,12 @@ class GRAPEArgs:
     node_dim: int = field(default=64)
 
 
-def grape(features):
-    grapeArgs = GRAPEArgs()
-    model = get_gnn(features, grapeArgs)
+def grape(features, dataset):
+    print(dataset)
+    load_path = './GRAPE/uci/mdi_results/results/gnn_mdi_v1/{}/0/impute_model.pt'\
+        .format(dataset)
+    #model = torch.nn.Module.load_state_dict(torch.load(load_path))
+#    grapeArgs = GRAPEArgs()
+    #model = get_gnn(features, grapeArgs)
     pass
 
