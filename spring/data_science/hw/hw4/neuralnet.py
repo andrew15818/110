eeccffc5c0cac5ae0500716d2a7db9919769a160
Andres Ponce <andreseeponce@gmail.com>
2022-05-06 03:08:57 +0800

    p6

--- spring/data_science/hw/hw4/neuralnet.py
+++ spring/data_science/hw/hw4/neuralnet.py
@@ -11,11 +11,17 @@ from torch.utils.data import Dataset, DataLoader
 class tabularDataset(Dataset):
     # Input data should already be scaled
     def __init__(self,data:pd.DataFrame):
-        pass
+        self.data = data
+        self.len = data.shape[0]
+
     def __len__(self):
-        pass
-    def __getitem__(self):
-        pass
+        return self.len
+
+    def __getitem__(self, idx):
+        features = self.data.iloc[idx,:-1]
+        label = self.data.iloc[idx, -1]
+        
+        return torch.tensor(features), torch.tensor(label)
 
 class MLP(nn.Module):
     def __init__(self, input_dims:int, class_num:int):
@@ -43,11 +49,27 @@ class tabularNet:
     def __init__(self):
         pass
     def fit(self, x:pd.DataFrame, y:pd.Series, n_labels=1, epochs=5, lr=0.001) -> float:
-        dims = x.shape[1]
+        dataset = tabularDataset(x)
+        dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+        dims = x.shape[1] - n_labels # Number of label columns in output 
         class_num = y.nunique()
+        epochs = 10
         model = MLP(dims, class_num)
-        pass
+        for i in range(epochs):
+            for feature, label in dataloader:
+                # Inference
+                feature = feature.float()
+                label = label.float()
+                print(label)
+                out = model(feature)
+
+                # Backprop
 
 if __name__ == '__main__':
-    tnet = tabularNet()
+    df = pd.read_csv('./data/biodeg.csv')
+    net = tabularNet()
+    net.fit(df.iloc[:, :-1], df.iloc[:, -1])
+
+
 
